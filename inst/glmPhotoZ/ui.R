11220a5d9edefcb76a1ad4711b6dcf7941909f93
algolkm <brain@auroratil-2.home>
2014-08-19 17:18:15 +0100

    Shiny application - UI
    Minor UI code changes.

--- inst/glmPhotoZ/ui.R
+++ inst/glmPhotoZ/ui.R
@@ -10,32 +10,24 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
 
     h4("Data Input"),
-
     checkboxInput('dataSourceFlag', 'Used PHAT0 data', FALSE),
-
     fileInput('file1', 'Data for training', accept=c('.dat', '.txt')),
-
     fileInput('file2', 'Data to estimate photoZ', accept=c('.dat', '.txt')),
-
     h4("Control and options"),
     selectInput("method", "Method:",
                 list("Frequentist" = "Frequentist", 
                      "Bayesian" = "Bayesian")),
-
     selectInput("family", "Family:",
                 list("Gamma" = "gamma", 
                      "Inverse Gaussian" = "inverse.gaussian")),
-
     sliderInput("fracDataDiag", "Train data fraction for diagnostics:", 
                 min = 0, max = 1, value = 0.2, step= 0.01),
-
     br(), 
     submitButton("Run analysis"),
     br(),
     downloadButton('downloadData', 'Download photoZ results')
 
   ),
-
 
   # Show output plot
   mainPanel(
