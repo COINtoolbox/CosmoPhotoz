c103ab7ae7848f92d247effb00d916adaa2ae4cc
algolkm <brain@auroratil-2.home>
2014-08-19 18:30:24 +0100

    Shiny application
    Addition of the status message. Major modifications in the data loading
    part of the server.

--- inst/glmPhotoZ/server.R
+++ inst/glmPhotoZ/server.R
@@ -1,100 +1,53 @@
 library(shiny)
 library(CosmoPhotoz)
-options(shiny.maxRequestSize=30*1024^2) # This is to change the maximum size of the upload to 30 MB
+options(shiny.maxRequestSize=100*1024^2) # This is to change the maximum size of the upload to 30 MB
 
 shinyServer(function(input, output) {
 
   # Now, we need to create the reactive container
   shinyCompPhotoZ <- reactive({
     # First we need to have some data, of course!
-
-    if(input$dataSourceFlag == FALSE) {
-      # Read and parse the file
-      inFile <- input$file1
-      if (is.null(inFile)) {
-        return(NULL)
+      if(input$dataSourceFlag == FALSE) {
+        # First the file used for training
+        inFile1 <- input$file1
+        if (is.null(inFile1)) {
+          return(NULL)
+        }
+        # Then the file used for estimating
+        inFile2 <- input$file2
+        if (is.null(inFile2)) {
+          return(NULL)
+          #return("\n GLM PhotoZ Estimator :: No file was uploaded for redshift estimation! ")
+        }
+        # Now read the files
+        PHAT0train <- read.table(file=inFile1$datapath, header=TRUE)
+        PHAT0test <- read.table(file=inFile2$datapath, header=TRUE)
+      } else {
+        # In this case, just lazy load the data from inside the package
+        data(PHAT0train)
+        data(PHAT0test)
       }
-      MegaZ <- read.table(file=inFile$datapath, header=TRUE)
-      MegaZ.mag<-MegaZ[,c("mag_u","mag_g","mag_r","mag_i","mag_z","redshift")]
 
-      ######
-      ###### PRELIMINARY :: Data preparation only
-      ######
-      set.seed(12378)
-      test_index <- sample(seq_len(nrow(MegaZ.mag)), replace=F, size = floor(input$fracDataDiag * nrow(MegaZ.mag)))
-      Mega.train <- as.data.frame(MegaZ.mag[-test_index, ]) # training set
-      Mega.test <- as.data.frame(MegaZ.mag[test_index, ])   # testing set
-
-      ######
-      ###### THE ACTUAL CODE IS HERE
-      ######
-      PC_comb<-computeCombPCA(Mega.train[,-6],Mega.test[,-6], npcvar=0.995)
-      specz<-Mega.test$redshift
-
-      # Principal componentes for training data
-      trainpc<-cbind(PC_comb$x, redshift=Mega.train$redshift)
-
-      # Principal components for test data
-      testpc<-PC_comb$y
-
-      # Here you train compute the glm model to predict photometric redshifts
-#      glmfit <- glmTrainPhotoZ(trainpc, formula=eval(parse(text="redshift~Comp.1*Comp.2*Comp.3*Comp.4")), method=input$method, family=input$family)
-#      glmfit <- glmTrainPhotoZ(trainpc, formula=redshift~Comp.1*Comp.2*Comp.3*Comp.4, method=input$method, family=input$family)
-#      glmfit <- glmTrainPhotoZ(trainpc, formula=redshift~Comp.1*Comp.2*Comp.3*Comp.4, method="Frequentist", family = "gamma")
-  #    formM <- paste(names(PC_comb$x), collapse="*")
- #     formM <- paste("redshift~",formM, sep="")
-#      glmfit <- glmTrainPhotoZ(trainpc, formula=eval(parse(text=formM)), method="Frequentist", family = "gamma")
-
-      # Dynamic generation of the formula based on the user selected number of PCs
-      formM <- paste(names(PC_comb$x[1:4]), collapse="*") ## THE NUMBER OF PCS USED ENTER HERE
-      formM <- paste("redshift~",formM, sep="")
-      # Fitting
-      Fit<-glmTrainPhotoZ(Trainpc, formula=eval(parse(text=formM)), method=input$method, family=input$family)
-
-      # Here you predict your photometric redshift from your photometric data
-      photoz <- glmPredictPhotoZ(data = testpc, train = glmfit$glmfit)$photoz
-    } else {
-      data(PHAT0train)
-      data(PHAT0test)
+      # Combine the training and test data and calculate the principal components
       PC_comb<-computeCombPCA(subset(PHAT0train, select=c(-redshift)),
                               subset(PHAT0test,  select=c(-redshift)),
                               npcvar=0.995)    
       Trainpc<-cbind(PC_comb$x, redshift=PHAT0train$redshift)
       Testpc<-PC_comb$y
+
       # Dynamic generation of the formula based on the user selected number of PCs
       formM <- paste(names(PC_comb$x[1:6]), collapse="*") ## THE NUMBER OF PCS USED ENTER HERE
       formM <- paste("redshift~",formM, sep="")
+      
       # Fitting
       Fit<-glmTrainPhotoZ(Trainpc, formula=eval(parse(text=formM)), method=input$method, family=input$family)
+      
       # Photo-z estimation
       photoz<-predict(Fit$glmfit, newdata=Testpc, type="response")
       specz<-PHAT0test$redshift
-    }
-
 
     # Time to return the data!
     return(data.frame(photoz, specz))
-  })
-
-  shinyCompPhotoZ_estimate <- reactive({
-    # First we need to have some data, of course!
-    inFile <- input$file2
-    if (is.null(inFile)) {
-      return("\n GLM PhotoZ Estimator :: No file was uploaded for redshift estimation! ")
-    }
-
-    #MegaZestimate <- read.table(file=inFile$datapath, header=TRUE)
-    #MegaZestimate.test <- MegaZestimate[,c("mag_u","mag_g","mag_r","mag_i","mag_z","redshift")]
-
-
-    # STILL NEED TO COMPUTE THE TEST DATA IN THE PC SPACE
-
-    # TODO TODO TODO
-
-    # Here you predict your photometric redshift from your photometric data
-    #photozRes <- glmPredictPhotoZ(data = testpc, train = glmfit$glmfit)$photoz
-
-    #return(photozRes)
   })
 
   # Create the output text
@@ -122,6 +75,12 @@ shinyServer(function(input, output) {
     temp <- shinyCompPhotoZ()
     if(!is.null(temp)) {
       plotDiagPhotoZ(temp$photoz, temp$specz, type = "errorviolins")
+    }
+  })
+  output$box <- renderPlot({
+    temp <- shinyCompPhotoZ()
+    if(!is.null(temp)) {
+      plotDiagPhotoZ(temp$photoz, temp$specz, type = "box")
     }
   })
 
--- inst/glmPhotoZ/ui.R
+++ inst/glmPhotoZ/ui.R
@@ -8,9 +8,26 @@ shinyUI(pageWithSidebar(
 
   # Sidebar with controls
   sidebarPanel(
+    tags$head(tags$style(type="text/css", "
+             #loadmessage {
+               position: fixed;
+               top: 50%;
+               left: 0px;
+               width: 100%;
+               padding: 5px 0px 5px 0px;
+               text-align: center;
+               font-weight: bold;
+               font-size: 125%;
+               color: #FFFFFF;
+               background-color: #B22222;
+               z-index: 105;
+             }
+          ")),
+           conditionalPanel(condition="$('html').hasClass('shiny-busy')",
+                            tags$div("Calculating... you can get a coffee.", id="loadmessage")),
 
     h4("Data Input"),
-    checkboxInput('dataSourceFlag', 'Used PHAT0 data', FALSE),
+    checkboxInput('dataSourceFlag', 'Use PHAT0 data', FALSE),
     fileInput('file1', 'Data for training', accept=c('.dat', '.txt')),
     fileInput('file2', 'Data to estimate photoZ', accept=c('.dat', '.txt')),
     h4("Control and options"),
@@ -35,6 +52,7 @@ shinyUI(pageWithSidebar(
       tabPanel("Error Distribution", plotOutput("errorDistPlot")), 
       tabPanel("Prediction", plotOutput("predictObs")), 
       tabPanel("Violins", plotOutput("violins")),
+      tabPanel("Box", plotOutput("box")),
       tabPanel("Diagnostics", verbatimTextOutput("diagnostics"))
     )
   )
