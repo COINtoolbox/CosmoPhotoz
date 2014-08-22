ac77d8959d1c67ed5894bbf93698df8317d00267
Rafael <rafael.2706@gmail.com>
2014-08-22 20:48:59 +0100

    remove my useless example and the html

--- R/CosmoPhotoz-package.R
+++ R/CosmoPhotoz-package.R
@@ -2,7 +2,7 @@
 #' @name PHAT0test
 #' @title PHAT0 test dataset
 #' @description The CosmoPhotoz package includes the dataset \code{PHAT0test}, containing photometric 
-#' data in the astronomical photometric bands up, gp, rp, ip, zp, Y, J, H, K, IRAC_1, IRAC_2
+#' data in the astronomical photometric bands u, g, r, i, z, Y, J, H, K, IRAC_1, IRAC_2
 #' and the redshift, for 161042 galaxies. The original PHAT0 dataset, containing 169520 was 
 #' split in two (\code{\link{PHAT0train}} and \code{\link{PHAT0test}}).
 #' @docType data
@@ -12,11 +12,11 @@
 #' \describe{
 #' 
 #' \item{\code{redshift}}{The redshift of the galaxy.}
-#' \item{\code{up}}{The magnitude of the galaxy in the up band.}
-#' \item{\code{gp}}{The magnitude of the galaxy in the gp band.}
-#' \item{\code{rp}}{The magnitude of the galaxy in the rp band.}
-#' \item{\code{ip}}{The magnitude of the galaxy in the ip band.}
-#' \item{\code{zp}}{The magnitude of the galaxy in the zp band.}
+#' \item{\code{u}}{The magnitude of the galaxy in the u band.}
+#' \item{\code{g}}{The magnitude of the galaxy in the g band.}
+#' \item{\code{r}}{The magnitude of the galaxy in the r band.}
+#' \item{\code{i}}{The magnitude of the galaxy in the i band.}
+#' \item{\code{z}}{The magnitude of the galaxy in the z band.}
 #' \item{\code{Y}}{The magnitude of the galaxy in the Y band.}
 #' \item{\code{J}}{The magnitude of the galaxy in the J band.}
 #' \item{\code{H}}{The magnitude of the galaxy in the H band.}
@@ -34,7 +34,7 @@ NULL
 #' @name PHAT0train
 #' @title PHAT0 train dataset
 #' @description The CosmoPhotoz package includes the dataset \code{PHAT0train}, containing photometric 
-#' data in the astronomical photometric bands up, gp, rp, ip, zp, Y, J, H, K, IRAC_1, IRAC_2
+#' data in the astronomical photometric bands u, g, r, i, z, Y, J, H, K, IRAC_1, IRAC_2
 #' and the redshift, for 8478 galaxies. The original PHAT0 dataset, containing 169520 was 
 #' split in two (\code{\link{PHAT0train}} and \code{\link{PHAT0test}}).
 #' @docType data
@@ -44,11 +44,11 @@ NULL
 #' \describe{
 #' 
 #' \item{\code{redshift}}{The redshift of the galaxy.}
-#' \item{\code{up}}{The magnitude of the galaxy in the up band.}
-#' \item{\code{gp}}{The magnitude of the galaxy in the gp band.}
-#' \item{\code{rp}}{The magnitude of the galaxy in the rp band.}
-#' \item{\code{ip}}{The magnitude of the galaxy in the ip band.}
-#' \item{\code{zp}}{The magnitude of the galaxy in the zp band.}
+#' \item{\code{u}}{The magnitude of the galaxy in the u band.}
+#' \item{\code{g}}{The magnitude of the galaxy in the g band.}
+#' \item{\code{r}}{The magnitude of the galaxy in the r band.}
+#' \item{\code{i}}{The magnitude of the galaxy in the i band.}
+#' \item{\code{z}}{The magnitude of the galaxy in the z band.}
 #' \item{\code{Y}}{The magnitude of the galaxy in the Y band.}
 #' \item{\code{J}}{The magnitude of the galaxy in the J band.}
 #' \item{\code{H}}{The magnitude of the galaxy in the H band.}
